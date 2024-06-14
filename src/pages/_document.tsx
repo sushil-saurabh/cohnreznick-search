55d1ac7ab61fddc73dcbc61828ca2b9a6f2976dd
sushil-saurabh <sushil.saurabh@icreon.com>
2024-06-14 16:26:22 +0530

    Chatbot-script-testing
    Chatbot-script-testing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,10 @@ export default function Document() {
         <link
           rel="icon"
           href="https://www.cohnreznick.com/-/media/themes/cohnreznick-sites/cohnreznick-site/cohnreznicktheme/images/favicon.ico"
+        />
+        <Script
+          strategy="afterInteractive"
+          src="https://assets.adoberesources.net/loader.js?orgId=B6AE2FB962AA82810A495F96%40AdobeOrg&instanceId=cohnreznick&env=prod&geo=va7"
         />
       </Head>
       <body className="open">
